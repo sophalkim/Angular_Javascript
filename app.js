d6a0e96d3c046eca9871041566e2f75e608d2279
Sophal Kim <sophal.kim.work@gmail.com>
2015-04-20 15:31:48 -0700

    Make sure to load script after DOM elements are loaded

---
+++ app.js
@@ -0,0 +1,34 @@
+(function () {
+	var app = angular.module('store', []);
+	var gem = {
+		name: 'ruby',
+		price: 4.99
+	};
+	app.controller('StoreController', function() {
+		this.product = gem;
+	});
+})();
+
+function $(id) {
+	return document.getElementById(id);
+}
+
+function changeText() {
+	$("textOne").innerHTML = "awesome";
+}
+
+function makeSizer(size) {
+  return function() {
+    document.body.style.fontSize = size + 'px';
+  };
+}
+
+var size12 = makeSizer(12);
+var size14 = makeSizer(14);
+var size16 = makeSizer(16);
+
+document.getElementById('size-12').onclick = function () {
+	alert("Size-12");
+};
+document.getElementById('size-14').onclick = size14;
+document.getElementById('size-16').onclick = size16;
