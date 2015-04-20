a12e4b6a0a01df96c976243ec1f8fb893e670c74
Sophal Kim <sophal.kim.work@gmail.com>
2015-04-20 16:53:51 -0700

    Module pattern worked with closure

--- app.js
+++ app.js
@@ -27,8 +27,35 @@ var size12 = makeSizer(12);
 var size14 = makeSizer(14);
 var size16 = makeSizer(16);
 
-document.getElementById('size-12').onclick = function () {
-	alert("Size-12");
-};
+document.getElementById('size-12').onclick = size12;
 document.getElementById('size-14').onclick = size14;
 document.getElementById('size-16').onclick = size16;
+
+var createPet = function(name) {
+	var sex;
+
+	return {
+		setName: function(newName) {
+			name = newName;
+		},
+
+		getName: function() {
+			return name;
+		},
+
+		getSex: function() {
+			return sex;
+		},
+
+		setSex: function(newSex) {
+			if(typeof newSex == "string" && (newSex.toLowerCase() == "male" || newSex.toLowerCase() == "female")) {
+				sex = newSex;
+			}
+		}
+	}
+}
+
+function changePet() {
+	var pet = createPet("Vivie");
+	$("petText").innerHTML = pet.getName();
+}
