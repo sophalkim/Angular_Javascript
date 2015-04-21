7d534982b60eedfecbc2e10fc7a4583944b7655a
Sophal Kim <sophal.kim.work@gmail.com>
2015-04-21 13:41:58 -0700

    Added images and scripts folder

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,9 @@
 	var app = angular.module('store', []);
 	var gem = {
 		name: 'ruby',
-		price: 4.99
+		price: 4.99,
+		canPurchase: true,
+		soldOut: false
 	};
 	app.controller('StoreController', function() {
 		this.product = gem;
