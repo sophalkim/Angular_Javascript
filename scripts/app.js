fa2faabd0e37f2c23a9197ce1ca53e7333fc002c
Sophal Kim <sophal.kim.work@gmail.com>
2015-04-21 22:21:24 -0700

    Added tabs functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,11 @@
 (function () {
+	// The module name is store and there are no dependencies
 	var app = angular.module('store', []);
-	
+	// Store Controller
+	app.controller('StoreController', function() {
+		this.products = gems;
+	});
+	// Data for the Store Controller
 	var gems = [
 		{
 			name: 'ruby',
@@ -24,17 +29,29 @@
 			image: "images/sapphire.jpg"
 		}
 	];
-
-	app.controller('StoreController', function() {
-		this.products = gems;
-	});
-
-	var gemlist = [
-		'opal', 'diamond', 'topaz', 'rainbow', 'princess', 'heart'
-	];
-
+	// Controller for the list of Gems, it will use with a filter to sortBy ascending
 	app.controller('GemListController', function() {
 		this.gemlist = gemlist;
 	});
+	// Data for GemListController
+	var gemlist = [
+		'opal', 'diamond', 'topaz', 'rainbow', 'princess', 'heart'
+	];
+	// Controller for controlling the tabs and information about the gems
+	app.controller('TabController', function() {
+		// Private int variable
+		this.tab = 1;
+		// Setter for tab variable
+		this.setTab = function(newValue) {
+			this.tab = newValue;
+			// console.log('The tab value is: ' + this.tab);
+		};
+		// Return true if the tabName is equal to tab
+		this.isSet = function(tabName) {
+			return this.tab === tabName;
+		};
+	});
+
+	
 
 })();
